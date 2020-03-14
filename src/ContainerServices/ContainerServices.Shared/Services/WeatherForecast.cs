d2a115252267b836865c6b315f4f1c156c6c702b
Rodrigo Moya <rodrigo@gnome.org>
2020-03-14 17:34:10 +0100

    [ContainerServices] Add helper method to retrieve WeatherForecast

--- src/ContainerServices/ContainerServices.Shared/Services/WeatherForecast.cs
+++ src/ContainerServices/ContainerServices.Shared/Services/WeatherForecast.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ContainerServices.Shared
 {
@@ -11,5 +14,14 @@ namespace ContainerServices.Shared
 		public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
 
 		public string Summary { get; set; }
+
+		public static async Task<WeatherForecast> FromUrlAsync (string url)
+		{
+			using var _httpClient = new HttpClient ();
+			var response = await _httpClient.GetAsync (url, HttpCompletionOption.ResponseHeadersRead);
+			response.EnsureSuccessStatusCode ();
+			var data = await response.Content.ReadAsStringAsync ();
+			return JsonConvert.DeserializeObject<WeatherForecast> (data);
+		}
 	}
 }
